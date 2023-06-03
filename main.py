e87aa4a1fe061160ab680c767c2b5260f7660864
SUSHANTH <sushanth172308@gmail.com>
2023-06-03 23:05:59 +0200

    data ingestion notebook

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from SummarizerText.logging import logger
+from SummarizerText.constants import *
+from SummarizerText.utils.common import *
 
 logger.info('Welcome to custum logging')
--- src/SummarizerText/utils/common.py
+++ src/SummarizerText/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from SummarizerTest.logging import logger
+from SummarizerText.logging import logger
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
