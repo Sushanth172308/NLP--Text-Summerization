c9c1749bb68d6aeb81f648acff44a7e00cafe814
SUSHANTH <sushanth172308@gmail.com>
2023-06-10 13:18:04 +0200

    data tranformation and model trainer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from SummarizerText.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from SummarizerText.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from SummarizerText.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 from SummarizerText.logging import logger
 
@@ -23,6 +24,19 @@ try:
 
 except Exception as e:
     logger.exception(e)
-    raise e     
+    raise e 
+
+
+STAGE_NAME ='Data Transformation stage'
+try:
+    logger.info(f'>>>>> stage{STAGE_NAME} started <<<<<<')
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f'>>>>> stage{STAGE_NAME} completed <<<<\n\nx========x')
+
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
 
 #logger.info('Welcome to custum logging')
