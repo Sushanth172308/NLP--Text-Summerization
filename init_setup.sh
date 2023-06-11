30025864e7777c9d620c66db2ccea706c7195e74
sushanth172308 <sushanth172308@gmail.com>
2023-06-11 12:34:16 +0200

    shell script

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda text_summerization_env with python 3.8" # change py version as per your need
-conda create --prefix ./env python=3.8 -y
+conda create --prefix ./text_summerization_env python=3.8 -y
 echo [$(date)]: "activate env"
 source activate ./env
 echo [$(date)]: "installing the requirements" 
